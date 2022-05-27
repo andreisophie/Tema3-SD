cd9cb651e6246fd7affae08a9c1f0f9d0e040ac0
andreisophie <andreisophie@yahoo.com>
2022-05-27 23:57:14 +0300

    Rearranged functions

--- tree.c
+++ tree.c
@@ -7,72 +7,30 @@
 #define NO_ARG ""
 #define PARENT_DIR ".."
 
-FileTree *createFileTree(char* rootFolderName)
+// *** FUNCTII APELATE DE COMENZI ***
+void touch(TreeNode* currentNode, char* fileName, char* fileText)
 {
-    FileTree *new_ft = malloc(sizeof(FileTree));
-    DIE(!new_ft, "malloc failed createFileTree: new_ft\n");
-
-    // alocare memorie folder
-    TreeNode *root = emptyDir(rootFolderName);
-    new_ft->root = root;
-
-    return new_ft;
-}
-
-// functie care elibereaza memoria alocata
-// pentru un fisier
-void freeFile(FileContent *file)
-{
-    free(file->text);
-    free(file);
-}
-
-// functie care elibereaza memoria alocata
-// pentru un folder
-void freeFolder(FolderContent *folder)
-{
-    ListNode *current = folder->children->head;
-    ListNode *next_node;
-    // eliminam memoria pentru fiecare folder/ fisier
-    // din interiorul folderului care se sterge
-    while (current) {
-        next_node = current->next;
-        freeNode(current->info);
-        free(current);
-        current = next_node;
+    if (!checkFile(currentNode, fileName)) {
+        return;
     }
-    free(folder->children);
-    free(folder);
-}
 
-// functie care elibereaza memoria alocata unui nod
-void freeNode(TreeNode *node)
-{
-    if (node->type == FILE_NODE)
-        freeFile(node->content);
+    TreeNode *newFile = malloc(sizeof(TreeNode));
+    DIE(!newFile, "malloc failed touch: newFile\n");
+
+    newFile->parent = currentNode;
+    newFile->name = fileName;
+    newFile->type = FILE_NODE;
+
+    FileContent *fileContent = malloc(sizeof(FileContent));
+    DIE(!fileContent, "malloc failed touch: fileContent\n");
+
+    if (fileContent)
+        fileContent->text = fileText;
     else
-        freeFolder(node->content);
-    free(node->name);
-    free(node);
-}
+        fileContent->text = NULL;
 
-// functie care elibereaza memoria alocata unui arbore
-void freeTree(FileTree *fileTree)
-{
-    freeNode(fileTree->root);
-    free(fileTree);
-}
-
-// functie care afiseaza folderele/ fisierele dintr-un
-// anumit director
-void ls_print_folder(TreeNode *currentNode)
-{
-    List *list = ((FolderContent *)currentNode->content)->children;
-    ListNode *node = list->head;
-    while (node) {
-        printf("%s\n", node->info->name);
-        node = node->next;
-    }
+    newFile->content = fileContent;
+    addChild(currentNode, newFile);
 }
 
 void ls(TreeNode* currentNode, char* arg)
@@ -109,66 +67,18 @@ void ls(TreeNode* currentNode, char* arg)
     }
 }
 
-void pwd(TreeNode* treeNode)
+void mkdir(TreeNode* currentNode, char* folderName)
 {
-    // s-a ajuns in root
-    if (!treeNode->parent) {
-        printf("%s", treeNode->name);
+    // exista deja un director cu numele introdus
+    if (!checkFile(currentNode, folderName)) {
+        printf("mkdir: cannot create directory '%s': File exists", folderName);
+        free(folderName);
         return;
     }
-
-    // se afiseaza recursiv fiecare director din cale
-    pwd(treeNode->parent);
-    printf("/%s", treeNode->name);
-}
-
-// functie care cauta un anumit nod in directorul actual
-TreeNode* search_node(TreeNode* currentNode, char* arg)
-{
-    List *list = ((FolderContent *)currentNode->content)->children;
-    ListNode *node = list->head;
-    while (node) {
-        if (strcmp(node->info->name, arg) == 0)
-            return node->info;
-        node = node->next;
-    }
-    if (!node)
-        return NULL;
-}
-
-// functie care returneaza directorul cautat dintr-o cale data
-TreeNode* searchDir(TreeNode* currentNode, char* path)
-{
-    char *pathCpy = strdup(path);
-    TreeNode *node = currentNode;
-    if (strcmp(pathCpy, "..") == 0) {
-        // am ajuns in root
-        if (currentNode->parent) {
-            free(pathCpy);
-            return currentNode->parent;
-        } else {
-            free(pathCpy);
-            return NULL;
-        }
-    }
-    // parsarea directoarelor pe care le parcurgem
-    char *token = strtok(pathCpy, "/\n\0");
-    while (token) {
-        if (strcmp(token, "..") == 0) {
-            node = node->parent;
-        } else {
-            node = search_node(node, token);
-            if (!node || node->type == FILE_NODE) {
-                // argument invalid, in cale trebuie sa
-                // existe doar directoare
-                free(pathCpy);
-                return NULL;
-            }
-        }
-        token = strtok(NULL, "/\0");
-    }
-    free(pathCpy);
-    return node;
+    // se creeaza noul director
+    TreeNode *newDir = emptyDir(folderName);
+    newDir->parent = currentNode;
+    addChild(currentNode, newDir);
 }
 
 TreeNode* cd(TreeNode* currentNode, char* path)
@@ -182,32 +92,6 @@ TreeNode* cd(TreeNode* currentNode, char* path)
     }
     // se modifica directorul curent
     return node;
-}
-
-// functie care afiseaza ierarhia de fisiere la apelarea comenzii "tree"
-void printTree(TreeNode* currentNode, int tabs, int *dirs, int *files)
-{
-    for (int i = 0; i < tabs; i++)
-        printf("\t");
-
-    printf("%s\n", currentNode->name);
-    if (currentNode->type == FILE_NODE) {
-        // am gasit un nou fisier, ne intorcem in director
-        // si continuam cautarea recursiva
-        (*files)++;
-        return;
-    }
-
-    // am gasit un nou director
-    (*dirs)++;
-    // parcurgem fisierele/ directoarele din acesta
-    List *list = ((FolderContent *)currentNode->content)->children;
-    ListNode *node = list->head;
-    while (node) {
-        // am gasit un nou fisier/ director pe care il vom parcurge
-        printTree(node->info, tabs + 1, dirs, files);
-        node = node->next;
-    }
 }
 
 void tree(TreeNode* currentNode, char* arg)
@@ -231,115 +115,17 @@ void tree(TreeNode* currentNode, char* arg)
     printf("%d directories, %d files", dirs, files);
 }
 
-// functie care aloca memorie pentru crearea unui director
-TreeNode *emptyDir(char* folderName)
+void pwd(TreeNode* treeNode)
 {
-    TreeNode *newDir = malloc(sizeof(TreeNode));
-    DIE(!newDir, "malloc failed createFileTree: root\n");
-
-    newDir->parent = NULL;
-    newDir->name = folderName;
-    newDir->type = FOLDER_NODE;
-
-    FolderContent *dirContent = malloc(sizeof(FolderContent));
-    DIE(!dirContent, "malloc failed createFileTree: FolderContent\n");
-
-    List* list = malloc(sizeof(List));
-    DIE(!list, "malloc failed createFileTree: FolderContent\n");
-
-    dirContent->children = list;
-    list->head = NULL;
-    newDir->content = dirContent;
-
-    return newDir;
-}
-
-// functie care verifica daca un fisier exista intr-un director
-int checkFile(TreeNode *currentNode, char* fileName)
-{
-    List *list = ((FolderContent *)currentNode->content)->children;
-    ListNode *node = list->head;
-    while (node) {
-        if (strcmp(node->info->name, fileName) == 0) {
-            return 0;
-        }
-        node = node->next;
-    }
-    return 1;
-}
-
-void mkdir(TreeNode* currentNode, char* folderName)
-{
-    // exista deja un director cu numele introdus
-    if (!checkFile(currentNode, folderName)) {
-        printf("mkdir: cannot create directory '%s': File exists", folderName);
-        free(folderName);
+    // s-a ajuns in root
+    if (!treeNode->parent) {
+        printf("%s", treeNode->name);
         return;
     }
-    // se creeaza noul director
-    TreeNode *newDir = emptyDir(folderName);
-    newDir->parent = currentNode;
-    addChild(currentNode, newDir);
-}
 
-void rmrec(TreeNode* currentNode, char* resourceName)
-{
-    // eliminam continutul directorul care va fi eliminat
-    List *list = ((FolderContent *)currentNode->content)->children;
-    ListNode *node = list->head;
-    ListNode *prev;
-    while (node) {
-        if (strcmp(node->info->name, resourceName) == 0) {
-            if (node == list->head) {
-                list->head = node->next;
-                break;
-            }
-            prev->next = node->next;
-            break;
-        }
-        prev = node;
-        node = node->next;
-    }
-    // nu exista directorul care se doreste sa fie eliminat
-    if (node == NULL) {
-        printf("rmrec: failed to remove '%s': No such file or directory",
-        resourceName);
-        return;
-    }
-    freeNode(node->info);
-    free(node);
-}
-
-void rm(TreeNode* currentNode, char* fileName)
-{
-    // cautam in director fisierul care trebuie eliminat
-    List *list = ((FolderContent *)currentNode->content)->children;
-    ListNode *node = list->head;
-    ListNode *prev;
-    while (node) {
-        if (strcmp(node->info->name, fileName) == 0) {
-            if (node->info->type == FOLDER_NODE) {
-                printf("rm: cannot remove '%s': Is a directory", fileName);
-                return;
-            }
-            if (node == list->head) {
-                list->head = node->next;
-                break;
-            }
-            prev->next = node->next;
-            break;
-        }
-        prev = node;
-        node = node->next;
-    }
-    // fisierul cu numele dorit nu exista
-    if (node == NULL) {
-        printf("rm: failed to remove '%s': No such file or directory",
-            fileName);
-        return;
-    }
-    freeNode(node->info);
-    free(node);
+    // se afiseaza recursiv fiecare director din cale
+    pwd(treeNode->parent);
+    printf("/%s", treeNode->name);
 }
 
 void rmdir(TreeNode* currentNode, char* folderName)
@@ -383,73 +169,64 @@ void rmdir(TreeNode* currentNode, char* folderName)
     free(node);
 }
 
-// functie care adauga un copil unui folder
-void addChild(TreeNode* folder, TreeNode *treeNode)
+void rm(TreeNode* currentNode, char* fileName)
 {
-    if (folder->type != FOLDER_NODE) {
-        printf("error addLast: param is not folder\n");
-        return;
-    }
-    List *list = ((FolderContent *)folder->content)->children;
-    ListNode *newNode = malloc(sizeof(ListNode));
-    DIE(!newNode, "malloc failed addLast: newNode\n");
-
-    newNode->info = treeNode;
-    newNode->next = list->head;
-    list->head = newNode;
-}
-
-void touch(TreeNode* currentNode, char* fileName, char* fileText)
-{
-    if (!checkFile(currentNode, fileName)) {
-        return;
-    }
-
-    TreeNode *newFile = malloc(sizeof(TreeNode));
-    DIE(!newFile, "malloc failed touch: newFile\n");
-
-    newFile->parent = currentNode;
-    newFile->name = fileName;
-    newFile->type = FILE_NODE;
-
-    FileContent *fileContent = malloc(sizeof(FileContent));
-    DIE(!fileContent, "malloc failed touch: fileContent\n");
-
-    if (fileContent)
-        fileContent->text = fileText;
-    else
-        fileContent->text = NULL;
-
-    newFile->content = fileContent;
-    addChild(currentNode, newFile);
-}
-
-// functie care returneaza fisierul de la calea data, daca exista
-TreeNode* searchFile(TreeNode* currentNode, char* path)
-{
-    char *pathCpy = strdup(path);
-    TreeNode *node = currentNode;
-    if (strcmp(pathCpy, "..") == 0) {
-        free(pathCpy);
-        return NULL;
-    }
-    char *token = strtok(pathCpy, "/\n\0");
-    while (token) {
-        if (strcmp(token, "..") == 0) {
-            node = node->parent;
-        } else {
-            node = search_node(node, token);
-            if (node && node->type == FILE_NODE) {
-                free(pathCpy);
-                return node;
+    // cautam in director fisierul care trebuie eliminat
+    List *list = ((FolderContent *)currentNode->content)->children;
+    ListNode *node = list->head;
+    ListNode *prev;
+    while (node) {
+        if (strcmp(node->info->name, fileName) == 0) {
+            if (node->info->type == FOLDER_NODE) {
+                printf("rm: cannot remove '%s': Is a directory", fileName);
+                return;
             }
-            if (!node) {
-                free(pathCpy);
-                return NULL;
+            if (node == list->head) {
+                list->head = node->next;
+                break;
             }
+            prev->next = node->next;
+            break;
         }
-        token = strtok(NULL, "/\0");
+        prev = node;
+        node = node->next;
     }
+    // fisierul cu numele dorit nu exista
+    if (node == NULL) {
+        printf("rm: failed to remove '%s': No such file or directory",
+            fileName);
+        return;
+    }
+    freeNode(node->info);
+    free(node);
+}
+
+void rmrec(TreeNode* currentNode, char* resourceName)
+{
+    // eliminam continutul directorul care va fi eliminat
+    List *list = ((FolderContent *)currentNode->content)->children;
+    ListNode *node = list->head;
+    ListNode *prev;
+    while (node) {
+        if (strcmp(node->info->name, resourceName) == 0) {
+            if (node == list->head) {
+                list->head = node->next;
+                break;
+            }
+            prev->next = node->next;
+            break;
+        }
+        prev = node;
+        node = node->next;
+    }
+    // nu exista directorul care se doreste sa fie eliminat
+    if (node == NULL) {
+        printf("rmrec: failed to remove '%s': No such file or directory",
+        resourceName);
+        return;
+    }
+    freeNode(node->info);
+    free(node);
 }
 
 void cp(TreeNode* currentNode, char* source, char* destination)
@@ -549,5 +326,238 @@ void mv(TreeNode* currentNode, char* source, char* destination)
     // mut nodul in noua cale
     fileCpy->parent = destNode;
     addChild(destNode, fileCpy);
+}
+
+// *** FUNCTII CARE CAUTA CAI ***
+
+// functie care cauta un anumit nod in directorul actual
+TreeNode* search_node(TreeNode* currentNode, char* arg)
+{
+    List *list = ((FolderContent *)currentNode->content)->children;
+    ListNode *node = list->head;
+    while (node) {
+        if (strcmp(node->info->name, arg) == 0)
+            return node->info;
+        node = node->next;
+    }
+    if (!node)
+        return NULL;
+}
+
+// functie care verifica daca un fisier exista intr-un director
+int checkFile(TreeNode *currentNode, char* fileName)
+{
+    List *list = ((FolderContent *)currentNode->content)->children;
+    ListNode *node = list->head;
+    while (node) {
+        if (strcmp(node->info->name, fileName) == 0) {
+            return 0;
+        }
+        node = node->next;
+    }
+    return 1;
+}
+
+// functie care returneaza directorul cautat dintr-o cale data
+TreeNode* searchDir(TreeNode* currentNode, char* path)
+{
+    char *pathCpy = strdup(path);
+    TreeNode *node = currentNode;
+    if (strcmp(pathCpy, "..") == 0) {
+        // am ajuns in root
+        if (currentNode->parent) {
+            free(pathCpy);
+            return currentNode->parent;
+        } else {
+            free(pathCpy);
+            return NULL;
+        }
+    }
+    // parsarea directoarelor pe care le parcurgem
+    char *token = strtok(pathCpy, "/\n\0");
+    while (token) {
+        if (strcmp(token, "..") == 0) {
+            node = node->parent;
+        } else {
+            node = search_node(node, token);
+            if (!node || node->type == FILE_NODE) {
+                // argument invalid, in cale trebuie sa
+                // existe doar directoare
+                free(pathCpy);
+                return NULL;
+            }
+        }
+        token = strtok(NULL, "/\0");
+    }
+    free(pathCpy);
+    return node;
+}
+
+// functie care returneaza fisierul de la calea data, daca exista
+TreeNode* searchFile(TreeNode* currentNode, char* path)
+{
+    char *pathCpy = strdup(path);
+    TreeNode *node = currentNode;
+    if (strcmp(pathCpy, "..") == 0) {
+        free(pathCpy);
+        return NULL;
+    }
+    char *token = strtok(pathCpy, "/\n\0");
+    while (token) {
+        if (strcmp(token, "..") == 0) {
+            node = node->parent;
+        } else {
+            node = search_node(node, token);
+            if (node && node->type == FILE_NODE) {
+                free(pathCpy);
+                return node;
+            }
+            if (!node) {
+                free(pathCpy);
+                return NULL;
+            }
+        }
+        token = strtok(NULL, "/\0");
+    }
+}
+
+// *** FUNCTII DE AFISARE ***
+
+// functie care afiseaza folderele/ fisierele dintr-un
+// anumit director
+void ls_print_folder(TreeNode *currentNode)
+{
+    List *list = ((FolderContent *)currentNode->content)->children;
+    ListNode *node = list->head;
+    while (node) {
+        printf("%s\n", node->info->name);
+        node = node->next;
+    }
+}
+
+// functie care afiseaza ierarhia de fisiere la apelarea comenzii "tree"
+void printTree(TreeNode* currentNode, int tabs, int *dirs, int *files)
+{
+    for (int i = 0; i < tabs; i++)
+        printf("\t");
+
+    printf("%s\n", currentNode->name);
+    if (currentNode->type == FILE_NODE) {
+        // am gasit un nou fisier, ne intorcem in director
+        // si continuam cautarea recursiva
+        (*files)++;
+        return;
+    }
+
+    // am gasit un nou director
+    (*dirs)++;
+    // parcurgem fisierele/ directoarele din acesta
+    List *list = ((FolderContent *)currentNode->content)->children;
+    ListNode *node = list->head;
+    while (node) {
+        // am gasit un nou fisier/ director pe care il vom parcurge
+        printTree(node->info, tabs + 1, dirs, files);
+        node = node->next;
+    }
+}
+
+// *** FUNCTII DE ALLOC/FREE ***
+
+// functie care initializeaza ierarhia de fisiere
+FileTree *createFileTree(char* rootFolderName)
+{
+    FileTree *new_ft = malloc(sizeof(FileTree));
+    DIE(!new_ft, "malloc failed createFileTree: new_ft\n");
+
+    // alocare memorie folder
+    TreeNode *root = emptyDir(rootFolderName);
+    new_ft->root = root;
+
+    return new_ft;
+}
+
+// functie care aloca memorie pentru crearea unui director
+TreeNode *emptyDir(char* folderName)
+{
+    TreeNode *newDir = malloc(sizeof(TreeNode));
+    DIE(!newDir, "malloc failed createFileTree: root\n");
+
+    newDir->parent = NULL;
+    newDir->name = folderName;
+    newDir->type = FOLDER_NODE;
+
+    FolderContent *dirContent = malloc(sizeof(FolderContent));
+    DIE(!dirContent, "malloc failed createFileTree: FolderContent\n");
+
+    List* list = malloc(sizeof(List));
+    DIE(!list, "malloc failed createFileTree: FolderContent\n");
+
+    dirContent->children = list;
+    list->head = NULL;
+    newDir->content = dirContent;
+
+    return newDir;
+}
+
+// functie care adauga un copil unui folder
+void addChild(TreeNode* folder, TreeNode *treeNode)
+{
+    if (folder->type != FOLDER_NODE) {
+        printf("error addLast: param is not folder\n");
+        return;
+    }
+    List *list = ((FolderContent *)folder->content)->children;
+    ListNode *newNode = malloc(sizeof(ListNode));
+    DIE(!newNode, "malloc failed addLast: newNode\n");
+
+    newNode->info = treeNode;
+    newNode->next = list->head;
+    list->head = newNode;
+}
+
+// *toate functiile elibereaza recursiv si copiii nodului
+
+// functie care elibereaza memoria alocata
+// pentru un fisier
+void freeFile(FileContent *file)
+{
+    free(file->text);
+    free(file);
+}
+
+// functie care elibereaza memoria alocata
+// pentru un folder
+void freeFolder(FolderContent *folder)
+{
+    ListNode *current = folder->children->head;
+    ListNode *next_node;
+    // eliminam memoria pentru fiecare folder/ fisier
+    // din interiorul folderului care se sterge
+    while (current) {
+        next_node = current->next;
+        freeNode(current->info);
+        free(current);
+        current = next_node;
+    }
+    free(folder->children);
+    free(folder);
+}
+
+// functie care elibereaza memoria alocata unui nod
+void freeNode(TreeNode *node)
+{
+    if (node->type == FILE_NODE)
+        freeFile(node->content);
+    else
+        freeFolder(node->content);
+    free(node->name);
+    free(node);
+}
+
+// functie care elibereaza memoria alocata unui arbore
+void freeTree(FileTree *fileTree)
+{
+    freeNode(fileTree->root);
+    free(fileTree);
 }
 
--- tree.h
+++ tree.h
@@ -42,44 +42,71 @@ struct List {
     ListNode* head;
 };
 
+// *** FUNCTII APELATE DE COMENZI ***
+void touch(TreeNode* currentNode, char* fileName, char* fileContent);
+
 void ls(TreeNode* currentNode, char* arg);
 
-void pwd(TreeNode* treeNode);
-
-TreeNode* searchDir(TreeNode* currentNode, char* path);
-
-TreeNode* searchFile(TreeNode* currentNode, char* path);
+void mkdir(TreeNode* currentNode, char* folderName);
 
 TreeNode* cd(TreeNode* currentNode, char* path);
 
 void tree(TreeNode* currentNode, char* arg);
 
-void addChild(TreeNode* folder, TreeNode *treeNode);
-
-void mkdir(TreeNode* currentNode, char* folderName);
-
-void rm(TreeNode* currentNode, char* fileName);
+void pwd(TreeNode* treeNode);
 
 void rmdir(TreeNode* currentNode, char* folderName);
 
+void rm(TreeNode* currentNode, char* fileName);
+
 void rmrec(TreeNode* currentNode, char* resourceName);
-
-void addLast(List *list, TreeNode *treeNode);
-
-void touch(TreeNode* currentNode, char* fileName, char* fileContent);
 
 void cp(TreeNode* currentNode, char* source, char* destination);
 
 void mv(TreeNode* currentNode, char* source, char* destination);
 
+// *** FUNCTII CARE CAUTA CAI ***
+// functie care cauta un anumit nod in directorul actual
+TreeNode* search_node(TreeNode* currentNode, char* arg);
+
+// functie care verifica daca un fisier exista intr-un director
+int checkFile(TreeNode *currentNode, char* fileName);
+
+// functie care returneaza directorul cautat dintr-o cale data
+TreeNode* searchDir(TreeNode* currentNode, char* path);
+
+// functie care returneaza fisierul de la calea data, daca exista
+TreeNode* searchFile(TreeNode* currentNode, char* path);
+
+// *** FUNCTII DE AFISARE ***
+// functie care afiseaza folderele/ fisierele dintr-un
+// anumit director
+void ls_print_folder(TreeNode *currentNode);
+
+// functie care afiseaza ierarhia de fisiere la apelarea comenzii "tree"
+void printTree(TreeNode* currentNode, int tabs, int *dirs, int *files);
+
+// functie care initializeaza ierarhia de fisiere
+FileTree *createFileTree(char* rootFolderName);
+
+// functie care aloca memorie pentru crearea unui director
 TreeNode *emptyDir(char* folderName);
 
-FileTree *createFileTree();
+// functie care adauga un copil unui folder
+void addChild(TreeNode* folder, TreeNode *treeNode);
 
+// *toate functiile elibereaza recursiv si copiii nodului
+
+// functie care elibereaza memoria alocata
+// pentru un fisier
 void freeFile(FileContent *file);
 
+// functie care elibereaza memoria alocata
+// pentru un folder
 void freeFolder(FolderContent *folder);
 
+// functie care elibereaza memoria alocata unui nod
 void freeNode(TreeNode *node);
 
+// functie care elibereaza memoria alocata unui arbore
 void freeTree(FileTree *fileTree);
